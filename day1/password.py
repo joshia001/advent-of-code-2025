8daa5790fa8d29421c586a3dd4a25c46822b72b5
joshia001 <joshi.aakash2001@gmail.com>
2026-01-11 15:03:51 +1100

    Added my day 1 solution

---
+++ day1/password.py
@@ -0,0 +1,89 @@
+"""_summary_
+Code that spins a dial left or right a certain number of steps. 
+The dial is numbered 0 to 99. This script counts the number of times the dial
+ends on a 0 after a certain instruction. 
+
+E.g an instruction could be "L32" which means spin the dial left 32 times.
+
+I probably could do something a lot simpler with the raw number of steps (some
+sort of addition and subtraction) but I thought it would be cool to simulate
+the dial as a doubly linked list.
+"""
+
+import pandas as pd
+
+class Node:
+    def __init__(self, val=None, prev=None, next=None):
+        self.val = val
+        self.prev = prev
+        self.next = next
+
+def insert_node_at_end(val, head):
+    # insert a new node at the end of the doubly linked list
+    new_node = Node(val=val, prev=head)
+    head.next = new_node
+    return new_node
+
+def create_cycle(root, tail):
+    root.prev = tail
+    tail.next = root
+
+def traverse(head, val):
+    visited = set()
+    while head not in visited:
+        visited.add(head)
+        if head.val==val:
+            return head
+        
+        # print("current node val: ", head.val)
+        # print("node.next: ", head.next.val)
+        # print("node.prev", head.prev.val)
+        
+        head = head.next
+    
+def create_dial(num, cycle=True, start_pos=0):
+    root = Node(val=0)
+    head = Node(val=1, prev=root)
+    root.next = head
+    
+    for i in range(2, num):
+        head = insert_node_at_end(i, head)
+        
+    if cycle == True:
+        create_cycle(root, head)
+        
+    start_node = traverse(root, start_pos)
+    
+    return start_node
+    
+    
+def main():
+    num = 100
+    start_pos = 50
+    input_file_path = "day1\day1input.txt"
+    
+    pointer = create_dial(num, start_pos=start_pos)
+    print(pointer)
+    
+    instructions = pd.read_csv(input_file_path, header=None)
+    
+    num_zeros = 0
+    
+    for instruction in instructions[0]:
+        steps = int(instruction[1:])
+        if instruction[0] == 'R':
+            for _ in range(steps):
+                pointer = pointer.next
+        else:
+            for _ in range(steps):
+                pointer = pointer.prev
+        if pointer.val == 0:
+            num_zeros += 1
+    
+    print("Number of times we ended on zero: ", num_zeros)
+
+            
+if __name__ == "__main__":
+    main()
+    
+    
