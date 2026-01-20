5d09269f17cb6db8d9a5ff1222aa4575e76bc0f6
joshia001 <joshi.aakash2001@gmail.com>
2026-01-20 23:44:02 +1100

    Attempted day 4 part 2 using recursive DFS. Slow and incorrect.

--- day4/paperrolls.py
+++ day4/paperrolls.py
@@ -1,3 +1,6 @@
+import sys
+sys.setrecursionlimit(10000)
+
 input_file_path = "day4/day4input.txt"
 limit = 4
 
@@ -5,8 +8,9 @@ with open(input_file_path) as f:
     grid = [line.rstrip("\n") for line in f]
 
 grid_size = (len(grid), len(grid[0]))
+print(grid_size)
 
-def get_neighbours(row, col, grid_size):
+def get_neighbours(row, col):
     neighbours = []
     directions = ((1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, -1))
     for d in directions:
@@ -14,25 +18,40 @@ def get_neighbours(row, col, grid_size):
             neighbours.append((row+d[0], col+d[1]))
     return neighbours
 
-def can_access(neighbours, grid, limit):
+def can_access(neighbours, removed):
     adj_rolls = 0
     for n in neighbours:
-        if grid[n[0]][n[1]] == "@":
+        if grid[n[0]][n[1]] == "@" and not n in removed:
             adj_rolls += 1
     return adj_rolls < limit
+
+def dfs(row, col, removed, visited):
+    visited.add((row, col))
+    neighbours = get_neighbours(row, col)
+    if can_access(neighbours, removed):
+            removed.add((row, col))
+            for n in neighbours:
+                if grid[n[0]][n[1]] == "@":
+                    if not (n[0], n[1]) in visited:
+                        dfs(n[0], n[1], removed, visited)
+
+                
+def main():
+    # num_can_access = 0
+    removed = set()
     
-def main(grid, grid_size, limit):
-    num_can_access = 0
     for row in range(len(grid)):
         for col in range(len(grid[0])):
             if grid[row][col] == "@":
-                neighbours = get_neighbours(row, col, grid_size)
-                if can_access(neighbours, grid, limit):
-                    num_can_access += 1 
+                neighbours = get_neighbours(row, col)
+                if can_access(neighbours, removed):
+                    # removed.add((row, col))
+                    visited = set()
+                    dfs(row, col, removed, visited)
 
-    print(f"Rolls that can be accessed by a forklift: {num_can_access}")
+    print(f"Rolls that can be accessed by a forklift: {len(removed)}")
             
 if __name__ == '__main__':
-    main(grid, grid_size, limit)
+    main()
         
 
