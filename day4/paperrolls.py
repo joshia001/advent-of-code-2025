9f738319a08e8d72a9ca4a5cf89610c2c455700c
joshia001 <joshi.aakash2001@gmail.com>
2026-01-21 01:14:27 +1100

    Attempted day 4 part 2 using a queue and hash map. Correct answer!

--- day4/paperrolls.py
+++ day4/paperrolls.py
@@ -1,5 +1,4 @@
-import sys
-sys.setrecursionlimit(10000)
+from collections import deque
 
 input_file_path = "day4/day4input.txt"
 limit = 4
@@ -8,50 +7,50 @@ with open(input_file_path) as f:
     grid = [line.rstrip("\n") for line in f]
 
 grid_size = (len(grid), len(grid[0]))
-print(grid_size)
 
 def get_neighbours(row, col):
-    neighbours = []
+    neighbours = set()
     directions = ((1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, -1))
     for d in directions:
         if 0 <= row + d[0] <= grid_size[0]-1 and 0 <= col + d[1] <= grid_size[1]-1:
-            neighbours.append((row+d[0], col+d[1]))
-    return neighbours
+            if grid[row + d[0]][col + d[1]] == "@":
+                neighbours.add((row+d[0], col+d[1]))
+    return neighbours, len(neighbours)
 
-def can_access(neighbours, removed):
+def num_adj_rolls(neighbours):
     adj_rolls = 0
     for n in neighbours:
-        if grid[n[0]][n[1]] == "@" and not n in removed:
+        if grid[n[0]][n[1]] == "@":
             adj_rolls += 1
-    return adj_rolls < limit
+    return adj_rolls
 
-def dfs(row, col, removed, visited):
-    visited.add((row, col))
-    neighbours = get_neighbours(row, col)
-    if can_access(neighbours, removed):
-            removed.add((row, col))
-            for n in neighbours:
-                if grid[n[0]][n[1]] == "@":
-                    if not (n[0], n[1]) in visited:
-                        dfs(n[0], n[1], removed, visited)
-
-                
 def main():
-    # num_can_access = 0
-    removed = set()
+    num_can_remove = 0
+    rolls_map = {}
+    queue = deque([])
     
     for row in range(len(grid)):
         for col in range(len(grid[0])):
             if grid[row][col] == "@":
-                neighbours = get_neighbours(row, col)
-                if can_access(neighbours, removed):
-                    # removed.add((row, col))
-                    visited = set()
-                    dfs(row, col, removed, visited)
-
-    print(f"Rolls that can be accessed by a forklift: {len(removed)}")
+                neighbours, adj_rolls = get_neighbours(row, col)
+                if adj_rolls < 4:
+                    queue.append([neighbours, adj_rolls])
+                else:
+                    rolls_map[(row, col)] = adj_rolls
+  
+    while queue:
+        res = queue.popleft()
+        num_can_remove += 1
+        for coord in res[0]:
+            if coord in rolls_map.keys():
+                rolls_map[coord] -= 1
+                if rolls_map[coord] < 4:
+                    neighbours, _ = get_neighbours(coord[0], coord[1])
+                    queue.append([neighbours, rolls_map[coord]])
+                    del rolls_map[coord]
+    print(grid)
+    
+    print(f"Rolls that can be accessed by a forklift: {num_can_remove}")
             
 if __name__ == '__main__':
     main()
-        
-
